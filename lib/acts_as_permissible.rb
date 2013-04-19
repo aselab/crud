ea219ff7609e3bf204b20c8cba103bf248b2a778
Akihiro Ono <akihiro@ase.co.jp>
2013-04-19 20:26:42 +0900

    Implemented acts_as_permissible

--- lib/acts_as_permissible.rb
+++ lib/acts_as_permissible.rb
@@ -9,13 +9,25 @@ module Permissible
     # アクセス権をビットで定義する
     # {:manage => 0b111, :write => 0b011, :read => 0b001, :default => 0b001}
     def acts_as_permissible(permissions)
+      self.define_singleton_method(:defined_permissions) { permissions }
+
       self.class_eval do
-        # Todo
+        has_many :permissions, :as => :permissible, :dependent => :destroy,
+          :before_add => :assign_default_flags
+
+        has_many :principals, :through => :permissions
+
+        accepts_nested_attributes_for :permissions, :allow_destroy => true
+        attr_accessible :permissions_attributes, :as => :admin
       end
     end
   end
 
   module InstanceMethods
+    private
+    def assign_default_flags(record)
+      record.flags ||= self.class.defined_permissions[:default]
+    end
   end
 
   class Railtie < ::Rails::Railtie #:nodoc:
