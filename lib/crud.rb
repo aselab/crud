ba03d97d302dde51f63478910418d270f54eb825
y-yoshinoya <y-yoshinoya@ase.co.jp>
2013-06-13 09:13:12 +0900

    select2をrequireする場所を変更

--- lib/crud.rb
+++ lib/crud.rb
@@ -2,7 +2,4 @@ require "crud/engine"
 require "acts_as_permissible"
 
 module Crud
-  class Engine < ::Rails::Engine
-    require 'select2-rails'
-  end
 end
--- lib/crud/engine.rb
+++ lib/crud/engine.rb
@@ -2,6 +2,7 @@ require 'jquery-rails'
 require 'cancan'
 require 'kaminari'
 require 'simple_form'
+require 'select2-rails'
 
 module Crud
   class Engine < ::Rails::Engine
