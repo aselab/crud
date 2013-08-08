b5b461a522ba995c30484eee08b58d73ebad1da6
Akihiro Ono <akihiro@ase.co.jp>
2013-08-08 15:22:12 +0900

    Redmineの都合に合わせてajaxメソッドを使わないように変更、formの下に追加するとスタイルが適用されてしまうのでbodyにappendするように

--- app/assets/javascripts/crud/modal-form.js
+++ app/assets/javascripts/crud/modal-form.js
@@ -1,13 +1,9 @@
 $(function() {
   $(document).on("click", "a[data-toggle='modal-form']", function() {
     var a = $(this);
-    $.ajax({
-      method: "GET",
-      url: a.attr("href"),
-      success: function(data) {
-        a.after(data);
-        a.attr("data-action", $("#modal_form form").attr("action"));
-      }
+    $.get(a.attr("href"), function(data) {
+      $(document.body).append(data);
+      a.attr("data-action", $("#modal_form form").attr("action"));
     });
     return false;
   });
