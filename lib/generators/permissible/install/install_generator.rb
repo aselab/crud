e026d81e9613312a86d790064c8f93c7921046eb
Akihiro Ono <akihiro@ase.co.jp>
2014-05-28 14:25:49 +0900

    refs #3850 acts_as_permissibleジェネレータのmongoid対応

--- lib/generators/permissible/install/install_generator.rb
+++ lib/generators/permissible/install/install_generator.rb
@@ -11,15 +11,20 @@ module Permissible
       class_option :orm, type: :string, default: "active_record"
 
       def create_models
-        inject_into_class("app/models/#{principal_name}.rb", User) do
-          "  has_many :#{permission_name.pluralize}, dependent: :destroy\n"
-        end
+        case orm
+        when :active_record
+          inject_into_class("app/models/#{principal_name}.rb", User) do
+            "  has_many :#{permission_name.pluralize}, dependent: :destroy\n"
+          end
 
-        template "permission.rb.erb", "app/models/#{permission_name}.rb"
+          template "permission.rb.erb", "app/models/#{permission_name}.rb"
+        when :mongoid
+          template "mongoid_permission.rb.erb", "app/models/#{permission_name}.rb"
+        end
       end
 
       def create_migrations
-        if options[:orm] == "active_record"
+        if orm == :active_record
           require 'rails/generators/active_record'
           self.class.send(:include, ActiveRecord::Generators::Migration)
           migration_template "migration.rb.erb", "db/migrate/create_#{permission_name.pluralize}.rb"
@@ -27,6 +32,10 @@ module Permissible
       end
 
       private
+      def orm
+        options[:orm].to_sym
+      end
+
       def principal_name
         options[:principal].underscore
       end
