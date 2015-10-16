1e8f7d4f103ed8ed9e5da7aaaac1ebde1fc3f8ff
Akihiro Ono <akihiro@ase.co.jp>
2015-10-16 11:31:58 +0900

    stored_paramsのテスト追加

--- spec/controllers/crud/application_controller_spec.rb
+++ spec/controllers/crud/application_controller_spec.rb
@@ -5,6 +5,30 @@ describe Crud::ApplicationController do
   let(:model) { }
   before { allow(controller).to receive(:model).and_return(model) }
 
+  describe "#stored_params", focus: true do
+    before { allow(controller).to receive(:params).and_return("a" => 3, "b" => 4, "c" => 5, "d" => 6) }
+    context "キー指定なし" do
+      before { expect(controller).to receive(:stored_params_keys).and_return([:a, :c]) }
+      it "stored_params_keysのパラメータが保持されること" do
+        expect(controller.send(:stored_params)).to eq(a: 3, c: 5)
+      end
+
+      it "引数のパラメータで上書きできること" do
+        expect(controller.send(:stored_params, c: 1, z: 8)).to eq(a: 3, c: 1, z: 8)
+      end
+    end
+
+    context "キー指定あり" do
+      it "指定したキーのパラメータが保持されること" do
+        expect(controller.send(:stored_params, :a, :d)).to eq(a: 3, d: 6)
+      end
+
+      it "引数のパラメータで上書きできること" do
+        expect(controller.send(:stored_params, :a, :d, x: 1, z: 8)).to eq(a: 3, d: 6, x: 1, z: 8)
+      end
+    end
+  end
+
   describe "#tokenize" do
     it "nilを指定した場合" do
       expect(controller.send(:tokenize, nil)).to match_array []
