b496387d1f813b5c879f76ec73812815843186bb
Akihiro Ono <akihiro@ase.co.jp>
2015-10-16 20:12:36 +0900

    サンプルをremote対応リンクに変更

--- spec/dummy/app/helpers/users_helper.rb
+++ spec/dummy/app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
   def users_name_html(resource, value)
-    link_to value, resource
+    link_to value, resource, remote: @remote
   end
 
   def users_group_html(resource, value)
-    link_to value.name, value if value
+    link_to value.name, value, remote: @remote if value
   end
 end
