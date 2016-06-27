ab41272267310f72ed6729aa24a57bc4befd1ecf
Akihiro Ono <akihiro@ase.co.jp>
2016-06-27 17:31:16 +0900

    crud_actionをhelper_methodに追加

--- app/controllers/crud/application_controller.rb
+++ app/controllers/crud/application_controller.rb
@@ -12,7 +12,7 @@ module Crud
   helper BootstrapHelper
   helper_method :model, :model_name, :model_key, :resources, :resource, :columns,
     :stored_params, :cancel_path, :column_key?, :association_key?, :sort_key?, :has_nested?,
-    :sort_key, :sort_order, :index_actions, :column_type, :can?, :cannot?
+    :sort_key, :sort_order, :index_actions, :column_type, :can?, :cannot?, :crud_action
 
   before_action :set_defaults, :only => [:index, :show, :new, :edit, :create, :update]
   before_action :before_index, :only => :index
