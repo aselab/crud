a64d5bb84ccbc5fd36de73b5dd72b1d55b3477ee
Takamasa Seki <t-seki@ase.co.jp>
2018-05-08 17:41:44 +0900

    refs #17 依存ライブラリーの追加とcoreuiインポートファイルの追加

--- spec/dummy/app/javascript/packs/application.js
+++ spec/dummy/app/javascript/packs/application.js
@@ -1,6 +1,7 @@
 import 'crud'
 import 'crud/inputs'
 import 'crud/fontawesome'
+import 'crud/coreui'
 import 'bootstrap'
 
 import './styles/main'
---
+++ webpacker/coreui.js
@@ -0,0 +1,2 @@
+import coreui from "@coreui/coreui/dist/js/coreui.min"
+import "@coreui/coreui/scss/coreui.scss"
