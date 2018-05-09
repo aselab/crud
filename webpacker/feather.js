06d0f7085e2a9b31e9030d0e53f60ffce160d4aa
Takamasa Seki <t-seki@ase.co.jp>
2018-05-09 17:10:22 +0900

    refs #17 ajax load時に変換できていなかったのでobserveするように

--- webpacker/feather.js
+++ webpacker/feather.js
@@ -2,15 +2,20 @@ import './feather/feather.scss'
 import feather from 'feather-icons/dist/feather'
 
 window.feater = feather
+
 $(() => {
-  Array.from(document.getElementsByClassName("feather"), element => {
-    const iconname = Array.from(element.classList).find(clazz => clazz.indexOf("f-") != -1)
-    
-    if(iconname == undefined)return
-    element.classList.remove(iconname)
-    element.parentNode.replaceChild(new DOMParser().parseFromString(
-      feather.icons[iconname.split("f-")[1]].toSvg(),
-      'image/svg+xml'
-    ).querySelector('svg'), element)
-  })
+  const replace = () => {
+    Array.from(document.getElementsByClassName("feather"), element => {
+      const iconname = Array.from(element.classList).find(clazz => clazz.indexOf("f-") != -1)
+      
+      if(iconname == undefined)return
+      element.classList.remove(iconname)
+      element.parentNode.replaceChild(new DOMParser().parseFromString(
+        feather.icons[iconname.split("f-")[1]].toSvg(),
+        'image/svg+xml'
+      ).querySelector('svg'), element)
+    })
+  }
+  $(document.body).on("crud:DOMChange", replace)
+  replace()
 })
--- webpacker/index.js
+++ webpacker/index.js
@@ -7,3 +7,9 @@ import './crud/crud.scss'
 window.$ = window.jQuery = jQuery
 window.Rails = Rails
 Rails.start()
+
+$(() => {
+  new MutationObserver(callback => {
+    $(document.body).trigger("crud:DOMChange")
+  }).observe(document.body, {childList: true, subtree: true})
+})
